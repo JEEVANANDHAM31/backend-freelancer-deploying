3c4519608193214997580f415b0d0cff8f2bd5f0
JEEVANANDHAM31 <padmavathisaravanan769@gmail.com>
2025-08-24 12:05:00 +0530

    Railway integrated

--- src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "https://deploying-kappa.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
